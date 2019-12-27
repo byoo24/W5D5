7077b9cbe45bb037ace4b9895fe96216fe86df23
byoo24 <byoo.dev@gmail.com>
2019-12-26 23:13:05 -0500

    finished phase 1

---
+++ CharAndBoolean/src/academy/learnprogramming/Main.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myUnicodeChar);
+
+        boolean myTrueBoolean = true;
+        boolean myFalseBoolean = false;
+
+    }
+}
---
+++ Operators/src/academy/learnprogramming/Main.java
@@ -0,0 +1,12 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int score = 10_000;
+        int levelCompleted = 8;
+        int bonusSet = 200;
+
+    }
+}
